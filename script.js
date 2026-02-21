cc0ded08b260caf915c7de4f921737b4a45e4454
Advaiy Praveen <advaiypraveen@gmail.com>
2026-02-21 14:27:08 +0530

    UI improvements

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ let count = 0;
 function increase(){
     count = count +1;
     document.getElementById("counter").innerText = count;
+    document.getElementById("counter").style.color = "blue";
+ 
 }
 
 function decrease(){
     if(count >0){
         count = count-1;
-        document.getElementById("counter").style.color = "white";
         document.getElementById("counter").innerText = count;
+        document.getElementById("counter").style.color = "blue";
+
     }
 
     else{
