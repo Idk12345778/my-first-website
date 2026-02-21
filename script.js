dfac36a6d9aa5bd584c7e1d87198b03b591f8347
Advaiy Praveen <advaiypraveen@gmail.com>
2026-02-21 19:23:08 +0530

    refactored code for ui colors

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let count = 0;
+
+function updatedisplay(){
+    const counterElement = document.getElementById("counter");
+    counterElement.innerText = count;
+
+    if(counterElement===0){
+        counterElement.style.color = "red";
+    }
+    else{
+        counterElement.style.color ="blue";
+    }
+}
 function increase(){
     count = count +1;
-    document.getElementById("counter").innerText = count;
-    document.getElementById("counter").style.color = "blue";
+    updatedisplay();
  
 }
 
 function decrease(){
     if(count >0){
         count = count-1;
-        document.getElementById("counter").innerText = count;
-        document.getElementById("counter").style.color = "blue";
-
     }
-
-    else{
-        document.getElementById("counter").style.color = "red";
-    }
+    updatedisplay();
     
 }
